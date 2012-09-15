2a625505dec51cd771fa2a2423a94778e801dbd8
castavridis <casocializes@gmail.com>
2012-09-15 16:59:36 -0500

    Parsing JSON document is working. We will have to rewrite a bit of the tmOAuth file. Maybe we shouldn't even use it and scrap using the straight up API since that was successful last time.

--- _/js/JSONParser.js
+++ _/js/JSONParser.js
@@ -1,10 +1,26 @@
-$(document).ready(function(){
-	$.getJSON('tweets.json', function(data){
-		var items = [];
+var CACHED_TWEETS_URL = 'http://localhost:8888/cityPulse/tweetlapse/tweets.json';
 
-		$.each(data, function(key, val){
-			items.push('<li>' + val + '</li>');
-		});
+$(document).ready(function(){
+	console.log("Ready.");
+	console.log("Attempting to get tweets.");
+	$.getJSON(CACHED_TWEETS_URL, function(data) {
 		
-	}).appendTo('body');
+		$.each (data, function(key, value) {
+			var userName = value.user.name;
+			var userImage = value.user.profile_image_url_https;
+			var userDescription = value.user.description;
+			
+			var tweet = value.text;
+			var tweetId = value.id;
+
+			var geo = value.geo;
+			var place = value.place;
+			var coordinates = value.coordinates;
+			var time = value.created_at;
+			
+			var html = '<div class="tweet" id="'+ tweetId +'"><div class="userInfo"><h1>'+ userName +'</h1><img src="'+ userImage +'" alt="'+ userName +'\'s profile picture."/><p>'+ userDescription +'</p></div><div class="tweetInfo"><h2>'+ tweet +'</h2> Time: ' + time + ' <div> Place: '+ place +' | Geo: ' + geo + ' | Coordinates: ' + coordinates + ' </div></div></div>'
+
+			$('body').append(html);
+		})
+	});
 });
